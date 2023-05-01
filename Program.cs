a3067e180c4e8741d35335caac81cd138137e7c4
Chorn4 <118160241+Chorn4@users.noreply.github.com>
2023-05-02 00:33:06 +0300

    Код від завдання 6

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Prakt5
+{
+    internal class Program
+    {
+        enum companys
+        {
+          //kyiv
+          Telemart = 1,
+          SoftServe,
+          SoftwareONE,
+          //Kharkiv
+          YouControl,
+          Rkeeper,
+          Lopan,
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Company names:\n");
+
+            for (int i = 1; i <= (int)companys.Lopan; i++)
+            {
+                Console.WriteLine($"{i})" + (companys)i);
+            }
+
+            Console.Write("\nEnter known companys (Separate numbers with a comma): ");
+
+            string Entered = Console.ReadLine();
+            string[] EnteredCompanysArr = Entered.Split(',');
+
+            int[] Transfered = new int[EnteredCompanysArr.Length];
+
+            for (int i = 0; i < EnteredCompanysArr.Length; i++)
+            {
+                Transfered[i] = int.Parse(EnteredCompanysArr[i]);
+            }
+
+            List<int> KnownCompanys = Transfered.ToList();
+
+            Console.WriteLine("\nKnown companys: ");
+
+            foreach (int i in KnownCompanys)
+            {
+                if (i <= 0 || i > (int)companys.Lopan)
+                {
+                    Console.WriteLine($"{i})Error");
+                }
+                else 
+                {
+                    Console.WriteLine($"{i}){(companys)i}");
+                }
+            }
+        }
+    }
+}
